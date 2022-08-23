9bbe2443472f7fb816f9aef50f31be913292065b
souravbsk <souravbsk01@gmail.com>
2022-08-24 01:15:34 +0600

    resturent

---
+++ js/app.js
@@ -0,0 +1,11 @@
+$(document).ready(function(){
+    $(".slider").slick({
+        slidesToShow: 3,
+        slidesToScroll: 1,
+        autoplay: true,
+        autoplaySpeed: 2000,
+        prevArrow:".prev-btn",
+        nextArrow:".next-btn"
+
+    })
+})
