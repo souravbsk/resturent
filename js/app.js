a4e63b0196b9b140647060bef7fa53ecbc390bda
souravbsk <souravbsk01@gmail.com>
2022-09-03 07:47:46 +0600

    full site ready

--- js/app.js
+++ js/app.js
@@ -5,7 +5,25 @@ $(document).ready(function(){
         autoplay: true,
         autoplaySpeed: 2000,
         prevArrow:".prev-btn",
-        nextArrow:".next-btn"
+        nextArrow:".next-btn",
+        responsive:[
+            {
+               breakpoint:992,
+               settings:{
+                slidesToShow:2,
+               }
+            },
+            {
+             breakpoint:768,
+             settings:{
+              slidesToShow:1,
+             }
+          }
+         ]
 
+    })
+
+    $('.nav-trigger').click(function () {
+        $('.content-warper').toggleClass("scaled")
     })
 })
